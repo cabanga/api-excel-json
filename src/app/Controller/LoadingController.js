5cb7dca2ff8cbcbb6453a3d01bdf3b78ce0934c2
cabanga <manucabanga@gmail.com>
2021-01-17 18:55:49 +0100

    This commit move files

--- src/app/Controller/LoadingController.js
+++ src/app/Controller/LoadingController.js
@@ -1,8 +1,10 @@
-const LoadingRepo = require('../Repositories/LoadingRepo')
+//const LoadingRepo = require('../Repositories/LoadingRepo')
+
+import { loadInvoice } from '../Repositories/LoadingRepo'
 
 exports.store = (req, res) => {
     try {
-        const read_file = LoadingRepo.readFile()
+        const read_file = loadInvoice()
         res.status(201).send(read_file)
     } catch (err) {
         res.status(500).send({errors: err})
--- src/app/Repositories/LoadingRepo.js
+++ src/app/Repositories/LoadingRepo.js
@@ -1,13 +1,74 @@
-module.exports = {
-    
-    readFile(){
+const xlsx = require('xlsx')
+const fs = require('fs')
+const filePath = './Files/invoices/loading'
+const pathSuccess = './Files/invoices/success/'
+const pathFailed = './Files/invoices/failed/'
 
-        let data = {
-            "nome": "William",
-            "idade": "28",
-            "contato": "https://twitter.com/_uillaz"
+
+//=========================================================================================
+export async function loadInvoice () {
+    let process_invoices = await checkExistFile()
+    return process_invoices
+}
+
+
+//=========================================================================================
+function checkExistFile(){
+    fs.readdir(filePath, function (err, files) {
+        if (files.length) {
+            let current_file = files[0]
+            readFile(current_file)
+        }else{
+            console.log("NENHUM FICHEIRO ENCONTRADO")
         }
+    })
+}
 
-        return data
+//=========================================================================================
+function readFile(file){
+    var workbook = xlsx.readFile(`${filePath}/${file}`, {celldates: true})
+    var first_ws = workbook.Sheets['Municipios']
+    var data = xlsx.utils.sheet_to_json(first_ws)
+
+    if (data.length) {
+        moveFile(file)
     }
 }
+
+//=========================================================================================
+function moveFile(file) {
+    fs.copyFileSync(`${filePath}/${file}`, `${pathSuccess}/${file}`)
+    fs.renameSync(`${pathSuccess}/${file}`, `${pathSuccess}/invoice-${Date.now()}.xlsx`)
+    
+    fs.unlinkSync(`${filePath}/${file}`)
+    createLogs('File moved to the success folder')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=========================================================================================
+function createLogs(data){
+    let filePath = './Files/logs.txt'
+    fs.appendFile(filePath, `${data}\n`, () => {})
+}
+
